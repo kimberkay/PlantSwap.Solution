a15c41373ea92c2a81133ddcae9af049e1e53797
Kim Brannian <brannian@gmail.com>
2022-02-25 10:04:07 -0800

    add models

---
+++ Models/Swap.cs
@@ -0,0 +1,39 @@
+namespace Swap.Models
+{
+  public class Swap
+  {
+    public int SwapId { get; set; }
+    public string IngredientToSwap { get; set; }
+
+    public string CuisineType { get; set; }
+
+    public string SwapOne { get; set; }
+
+    public string SwapOneAbout { get; set; }
+
+    public string SwapOneFlavorNotes { get; set; }
+
+    public string SwapOnePrep { get; set; }
+
+    public string SwapTwo { get; set; }
+
+    public string SwapTwoAbout { get; set; }
+
+    public string SwapTwoFlavorNotes { get; set; }
+
+    public string SwapTwoPrep { get; set; }
+
+    public string SwapThree { get; set; }
+
+    public string SwapThreeAbout { get; set; }
+
+    public string SwapThreeFlavorNotes { get; set; }
+
+    public string SwapThreePrep { get; set; }
+  }
+}
+
+
+
+
+
