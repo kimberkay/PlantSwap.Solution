9ec924109ab361c3e4622c7145b37939d23dc05d
Kim Brannian <brannian@gmail.com>
2022-02-25 10:46:40 -0800

    add swap controllers

---
+++ Controllers/SwapsController.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Cors;
+using PlantSwap.Models;
+
+namespace HikingTrail.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class SwapsController : ControllerBase
+  {
+    private readonly PlantSwapContext _db;
+    public SwapsController(PlantSwapContext db)
+    {
+      _db = db;
+    }
+
+    // GET: api/trails
+    [EnableCors("outside")]
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Swap>>>Get(string ingredientToSwap, string cuisineType)
+    {
+      var query = _db.Swaps.AsQueryable();
+
+      if (ingredientToSwap != null)
+      {
+        query = query.Where(entry => entry.IngredientToSwap == ingredientToSwap);
+      }
+      
+      if (cuisineType != null)
+      {
+        query = query.Where(entry => entry.CuisineType == cuisineType);
+      }
+
+      return await query.ToListAsync();
+    }
+
+    // GET: api/trails/1
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Swap>>GetSwap(int id)
+    {
+      var swap = await _db.Swaps.FindAsync(id);
+
+      if (swap == null)
+      {
+        return NotFound();
+      }
+
+      return swap;
+    } 
+
+  }
+
+}
